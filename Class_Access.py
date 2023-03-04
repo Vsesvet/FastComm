20083760f7dd3a77b119afade9955c31ab457d60
Vsesvet <vsesvet@bk.ru>
2023-03-04 21:48:53 +0300

    Переработан Login

--- Class_Access.py
+++ Class_Access.py
@@ -1,16 +1,16 @@
 import sys
+from db_config import *
 from Class_Journal import *
 from Class_Mysql import *
 
 class Access:
     """Access module. Проверка данных Входа и роли пользователя, для совершения действия"""
+    def __init__(self):
+        self.mysql = Mysql()
     def login(self, input_phone, input_password):
         """Сверяет данные логина и пароля"""
-
-
-        datadb = Mysql(host, port, user, password, db_name)
         table_name = "users"
-        db = datadb.select_all(table_name)
+        db = self.mysql.select_all(table_name)
         for db_user in db:
             # Не верный пароль flag_access == 2.
             if db_user['phone_number'] == input_phone and db_user['password'] != input_password:
@@ -35,11 +35,8 @@ class Access:
 
     def get_username_and_role(self, user_login):
         username = f"{user_login['second_name']} {user_login['first_name']}"
-        datadb = Mysql(host, port, user, password, db_name)
-        role_id = user_login['role_id']
-        print(role_id)
-        role = datadb.get_role_by_role_id(role_id)
-        username_login_role = f'{username} ({role})'
+        role_name = self.mysql.get_role_by_role_id(user_login['role_id'])
+        username_login_role = f'{username} ({role_name})'
         return username_login_role
 
     def check_role_sysadmin(self, user_login):
@@ -78,13 +75,3 @@ class Access:
 
 
 journal = Journal()
-
-
-# db_user1 = {'phone_number': '79777194310', 'second_name': 'Ефремов', 'first_name': 'Максим', 'last_name': 'Георгиевич', 'role': 'sysadmin', 'fullname': 'Ефремов Максим Георгиевич', 'city': 'Москва', 'e-mail': 'efremov@mgido.com', 'password': 'gnt6al47'}
-# db_user2 = {'phone_number': '79296062723', 'second_name': 'Фомина', 'first_name': 'Ирина', 'last_name': 'Владимировна', 'role': 'admin', 'fullname': 'Фомина Ирина Владимировна', 'city': 'Москва', 'e-mail': 'fomina@mgido.com', 'password': 'gnt6al47'}
-# db_user3 = {'phone_number': '79671574048', 'second_name': 'Корнеева', 'first_name': 'Ирина', 'last_name': 'Алексеевна', 'role': 'admin', 'fullname': 'Корнеева Ирина Алексеевна', 'city': 'Москва', 'e-mail': 'i.korneeva@mgido.com', 'password': 'gnt6al47'}
-# db_user4 = {'phone_number': '79216436255', 'second_name': 'Дериглазова', 'first_name': 'Марина', 'last_name': 'Сергеевна', 'role': 'participant', 'fullname': 'Дериглазова Марина Сергеевна', 'city': 'Санкт-Петербург', 'e-mail': 'der-maria@yandex.ru', 'password': 'gnt6al47'}
-#
-#
-# # Database storage:
-# db = (db_user1, db_user2, db_user3, db_user4)
